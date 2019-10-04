831425a9c207012a8157c120f25911f5565dcc93
shinjisoo <jisoo.shin.87@gmail.com>
2019-10-04 18:09:08 +0900

    Change log&parameter directory

--- resnet/cifar_100_ResNet.py
+++ resnet/cifar_100_ResNet.py
@@ -3,13 +3,19 @@ import os
 import sys
 import math
 
-import nni 
+import nni
 
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
 from datetime import datetime
+
+base_path = os.environ['HOME'] + '/ResNet'
+if os.path.isdir(base_path):
+  pass
+else:
+  os.mkdir(base_path)
 
 data = tf.keras.datasets.cifar100.load_data()
 
@@ -22,7 +28,7 @@ test_image, test_label= test[0].astype(np.float32), test[1]
 train_label = np.reshape(train_label, newshape=[-1])
 test_label = np.reshape(test_label, newshape=[-1])
 
-# Generate Dataset obj   
+# Generate Dataset obj
 dataset_obj = tf.data.Dataset.from_tensors(
     {'image': train_image, 'label': train_label})
 dataset_obj = dataset_obj.shuffle(50000)
@@ -56,7 +62,7 @@ def _preprocessing(dataset, train_mode):
     image = tf.image.random_flip_left_right(image=image)
   else:
     image = dataset['image']
-    
+
   label = dataset['label']
   image = tf.math.subtract(
       x=image,
@@ -92,26 +98,26 @@ def residual_block(data, name, weight_decay):
   """
   with tf.name_scope(name) as scope:
     identity_data = data
-    
+
     data = keras.layers.Conv2D(
         filters=64, kernel_size=[1, 1], strides=1, padding='same',
         kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(data)
     data = keras.layers.BatchNormalization()(data)
     data = keras.layers.ReLU()(data)
-    
+
     data = keras.layers.Conv2D(
         filters=64, kernel_size=[3, 3], strides=1, padding='same',
         kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(data)
     data = keras.layers.BatchNormalization()(data)
     data = keras.layers.ReLU()(data)
-    
+
     data = keras.layers.Conv2D(
         filters=256, kernel_size=[1, 1], strides=1, padding='same',
         kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(data)
     data = keras.layers.BatchNormalization()(data)
-    
+
     data = keras.layers.Add()([data, identity_data])
-    
+
     data = keras.layers.ReLU()(data)
     return data
 
@@ -135,26 +141,26 @@ for layers in range(NUMBER_OF_LAYERS):
 
 with tf.name_scope("GlobalAveragePooling") as scope:
   x = keras.layers.GlobalAveragePooling2D()(x)
-  outputs = keras.layers.Dense(100, activation='softmax', 
+  outputs = keras.layers.Dense(100, activation='softmax',
       kernel_regularizer=tf.keras.regularizers.l2(wdc))(x)
 
 model = keras.Model(
-    inputs, outputs, 
+    inputs, outputs,
     name="{}layer_{}weight_decay_{}".format(
       NUMBER_OF_LAYERS, wdc,
-      datetime.strftime(datetime.now(), "%Y%m%d-%H")))
+      datetime.strftime(datetime.now(), "%Y%m%d-%H%M")))
 
 # Callbacks
 callbacks_list = [
   keras.callbacks.EarlyStopping(
-    monitor='val_sparse_categorical_accuracy', min_delta=1e-2, 
+    monitor='val_sparse_categorical_accuracy', min_delta=1e-2,
     patience=1000, verbose=1),
   keras.callbacks.ModelCheckpoint(
-      filepath='./resnet/ckpts/{}.h5'.format(model.name),
+      filepath=base_path+'/resnet/ckpts/{}.h5'.format(model.name),
       verbos=1, save_best_only=True
   ),
   keras.callbacks.TensorBoard(
-      log_dir='./resnet/logs/{}/'.format(model.name),
+      log_dir=base_path+'/logs/{}/'.format(model.name),
       histogram_freq=10,
       update_freq='epoch')]
 
@@ -171,9 +177,6 @@ model.fit(
   steps_per_epoch=100,
   validation_steps=100)
 
+
 model.evaluate(
-x=test_dataset.batch(1000))
-
-
-
-
+  x=test_dataset.batch(1000))
